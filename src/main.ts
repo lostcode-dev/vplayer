c87555fd665b5a9404d2bf59f273072fef9501a1
Daniel Soares <daniel.soares@fortics.com.br>
2023-05-23 08:54:30 -0300

    feature: add fake api.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { makeServer } from "./server"
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+makeServer()
 
 app.use(createPinia())
 app.use(router)

577009ba9ce6a61286328b8b8840888e7e577d0a
Daniel Soares <daniel.soares@fortics.com.br>
2023-05-23 19:09:48 -0300

    feature: Adjust in type.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { makeServer } from "./server"
+import { makeServer } from "./server.js"
 
 import App from './App.vue'
 import router from './router'

0049e763b19ed63289c50bd9033ee9261010c185
Isaac Vieira <isaacvf.dev@gmail.com>
2023-06-10 17:11:42 -0300

    Salvar recentes modificações no header

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -2,25 +2,30 @@
   <header
     class="w-full bg-emerald-50 fixed z-20"
   >
-    <nav class="flex justify-between items-center mx-8 px-4 py-5 font-semibold">
+    <nav class=" p-5 md:flex md:justify-between md:items-center font-semibold">
       <div>
-        <img class="w-16 cursor-pointer" src="../assets/logo.png" alt="Logo da empresa" id="logo" href="#">
+        <span class="text-2xl font-[Poppins] cursor-pointer">
+          <img class="h-10 inline " src="../assets/logo.png" alt="Logo da empresa" id="logo" href="#">
+          Raydan
+        </span>
       </div>      
       <div>
-        <ul class="flex justify-between items-center gap- [4vw]" >
-          <li>
-            <a class="hover:text-gray-500" href="">
+        <ul class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-emerald-50 w-full left-0 md:w-auto md:py-0 py-3 md:pl-0 pl-3 md:opacity-100">
+          <li class="mx-8 my-6 md:my-0">
+            <a class="text-lg font-semibold hover:text-teal-500 duration-500" href="">
               Ajuda
             </a>            
           </li>
-          <li>
-            <router-link class="hover:text-gray-500" :to="{ name: 'login'}">
+          <li class="mx-8 my-6 md:my-0">
+            <router-link class="text-lg font-semibold hover:text-teal-500 duration-500" :to="{ name: 'login'}">
               Login
             </router-link>
           </li>
-          <li>
-            <a-button class="ml-4">Seja o primeiro a saber</a-button>
-          </li>
+          
+          <a-button class="text-lg mx-8">
+            Seja o primeiro a saber
+          </a-button>
+          
         </ul>
       </div>
     </nav>

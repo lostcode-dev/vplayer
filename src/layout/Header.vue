9c737bc3a5b794cb1e7b2839719e199f8e5e43f3
Isaac Vieira <isaacvf.dev@gmail.com>
2023-06-10 11:45:09 -0300

    Primeira modificações no hearder

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -2,23 +2,26 @@
   <header
     class="w-full bg-emerald-50 fixed z-20"
   >
-    <nav class="flex justify-between mx-8 px-4 py-5 font-semibold">
+    <nav class="flex justify-between items-center mx-8 px-4 py-5 font-semibold">
       <div>
-        <a class="rounded-lg px-4 py-2 font-semibold" href="">
-          AlgumaLogo
-        </a>
-      </div>
-      <div class="font-semibold">
-
-      </div>
+        <img class="w-16 cursor-pointer" src="../assets/logo.png" alt="Logo da empresa" id="logo" href="#">
+      </div>      
       <div>
-        <a class="rounded-lg px-4 py-2 font-semibold" href="">
-          Ajuda
-        </a>
-        <router-link class="rounded-lg px-4 py-2 font-semibold" :to="{ name: 'login'}">
-          Login
-        </router-link>  
-        <a-button class="ml-4">Seja o primeiro a saber</a-button>
+        <ul class="flex justify-between items-center gap- [4vw]" >
+          <li>
+            <a class="hover:text-gray-500" href="">
+              Ajuda
+            </a>            
+          </li>
+          <li>
+            <router-link class="hover:text-gray-500" :to="{ name: 'login'}">
+              Login
+            </router-link>
+          </li>
+          <li>
+            <a-button class="ml-4">Seja o primeiro a saber</a-button>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>

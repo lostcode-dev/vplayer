9f9bec455cbc4d4455f5d4bd325854253c232534
Daniel Soares <daniel.soares@fortics.com.br>
2023-06-12 20:07:32 -0300

    feature(logo/3): Add fake logo.

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -3,6 +3,7 @@ import PlayAutoSmart from './PlayAutoSmart.vue'
 import ProgressBar from './ProgressBar.vue'
 import ResumePlay from './ResumePlay.vue'
 import Thumbnails from './Thumbnails.vue'
+import Logo from './Logo.vue'
 
 
 export default function install(app) {
@@ -11,4 +12,5 @@ export default function install(app) {
   app.component('a-progress-bar', ProgressBar)
   app.component('a-resume-play', ResumePlay)
   app.component('a-thumbnails', Thumbnails)
+  app.component('a-logo', Logo)
 }
--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,43 +1,35 @@
 <template>
-  <header
-    class="w-full bg-emerald-50 fixed z-20"
-  >
-    <nav class=" p-5 md:flex md:justify-between md:items-center font-semibold">
+  <header class="w-full bg-emerald-50 fixed z-20">
+    <nav class="p-4 md:flex md:justify-between md:items-center font-semibold">
       <div class="flex justify-between items-center">
         <span class="text-2xl font-[Poppins] cursor-pointer">
-          <img class="h-10 inline " src="../assets/logo.png" alt="Logo da empresa" id="logo" href="#">
-          Raydan
+          <a-logo />
         </span>
 
-        <span 
-          @click="toggleMenu"
-          class=" text-3xl cursor-pointer mx-2 md:hidden block" 
-          >
+        <span @click="toggleMenu" class="text-3xl cursor-pointer mx-2 md:hidden block">
           <i-navbar-menu v-if="!isMenuVisible" :width="20" :height="20" />
           <i-pause v-else :width="20" :height="20" />
         </span>
-      </div>      
+      </div>
       <div>
-        <ul        
-        class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-emerald-50 w-full left-0 md:w-auto md:py-0 py-3 md:pl-0 pl-3"
-        :class="{ 'hidden md:block': !isMenuVisible }"
-        @click="isMenuVisible = false"
+        <ul
+          class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-emerald-50 w-full left-0 md:w-auto md:py-0 py-3 md:pl-0 pl-3"
+          :class="{ 'hidden md:block': !isMenuVisible }"
+          @click="isMenuVisible = false"
         >
           <li class="mx-8 my-6 md:my-0">
-            <a class="text-lg font-semibold hover:text-teal-500 duration-500" href="">
-              Ajuda
-            </a>            
+            <a class="text-lg font-semibold hover:text-teal-500 duration-500" href=""> Ajuda </a>
           </li>
           <li class="mx-8 my-6 md:my-0">
-            <router-link class="text-lg font-semibold hover:text-teal-500 duration-500" :to="{ name: 'login'}">
+            <router-link
+              class="text-lg font-semibold hover:text-teal-500 duration-500"
+              :to="{ name: 'login' }"
+            >
               Login
             </router-link>
           </li>
-          
-          <a-button class="text-lg mx-8">
-            Seja o primeiro a saber
-          </a-button>
-          
+
+          <a-button class="text-lg mx-8"> Seja o primeiro a saber </a-button>
         </ul>
       </div>
     </nav>
@@ -45,24 +37,20 @@
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 export default {
-   
   setup() {
-    const isMenuVisible = ref(true);
-     
+    const isMenuVisible = ref(true)
+
     const toggleMenu = () => {
-      isMenuVisible.value = !isMenuVisible.value;
+      isMenuVisible.value = !isMenuVisible.value
     }
 
-  return {    
-    toggleMenu,
-    isMenuVisible
-  };
-    
+    return {
+      toggleMenu,
+      isMenuVisible
+    }
   }
 }
-  
-
 </script>

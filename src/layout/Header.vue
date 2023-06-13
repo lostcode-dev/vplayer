7399d3ca5f01efa3e898545290a180164e6e51aa
Isaac Vieira <isaacvf.dev@gmail.com>
2023-06-12 21:57:13 -0300

    Corrigido icone de fechar menu, adicionado mouseLeave para fechar menu ao tirar o mouse da ul. Adicionado NavbarCloseMenu.vue.

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -14,14 +14,15 @@
           class=" text-3xl cursor-pointer mx-2 md:hidden block" 
           >
           <i-navbar-menu v-if="!isMenuVisible" :width="20" :height="20" />
-          <i-pause v-else :width="20" :height="20" />
+          <i-navbar-close-menu v-else :width="20" :height="20" />
         </span>
       </div>      
       <div>
         <ul        
         class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-emerald-50 w-full left-0 md:w-auto md:py-0 py-3 md:pl-0 pl-3"
-        :class="{ 'hidden md:block': !isMenuVisible }"
+        :class="{ 'hidden': !isMenuVisible, 'block': isMenuVisible }"
         @click="isMenuVisible = false"
+        @mouseleave="hideMenu"
         >
           <li class="mx-8 my-6 md:my-0">
             <a class="text-lg font-semibold hover:text-teal-500 duration-500" href="">
@@ -45,20 +46,43 @@
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount  } from 'vue';
 
 export default {
    
   setup() {
-    const isMenuVisible = ref(true);
+    const isMenuVisible = ref(false);
+    
      
     const toggleMenu = () => {
       isMenuVisible.value = !isMenuVisible.value;
     }
 
+    const hideMenu = () => {
+      isMenuVisible.value = false;
+    };
+
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        isMenuVisible.value = true;
+      } else {
+        isMenuVisible.value = false;
+      }
+    };
+
+    onMounted(() => {
+      handleResize();
+      window.addEventListener('resize', handleResize);
+    });
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('resize', handleResize);
+    });
+
   return {    
     toggleMenu,
-    isMenuVisible
+    isMenuVisible,
+    hideMenu,
   };
     
   }

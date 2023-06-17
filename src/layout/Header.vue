be1f9979babc237cfb774268b4109aef09fddae0
Isaac Vieira <isaacvf.dev@gmail.com>
2023-06-16 21:39:20 -0300

    Corrigido click no meu em telas menores, deixado apenas para ser fechado ao clicar no icone do X. Retirado a função handleResize e OnMouted. O @mouseLeave está controlando a exibição do menu

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,5 +1,8 @@
 <template>
-  <header class="w-full bg-emerald-50 fixed z-20">
+  <header 
+  class="w-full bg-emerald-50 fixed z-20"
+  @mouseleave="hideMenu"
+  >
     <nav class="p-4 md:flex md:justify-between md:items-center font-semibold">
       <div class="flex justify-between items-center">
         <a  class="ml-6" href="">
@@ -14,9 +17,7 @@
       <div>
         <ul        
         class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-emerald-50 w-full left-0 md:w-auto md:py-0 py-3 md:pl-0 pl-3"
-        :class="{ 'hidden': !isMenuVisible, 'block': isMenuVisible }"
-        @click="isMenuVisible = false"
-        @mouseleave="hideMenu"
+        :class="{ 'hidden': !isMenuVisible, 'block': isMenuVisible }"      
         >
           <li class="mx-8 my-6 md:my-0">
             <router-link class="text-lg font-semibold hover:text-teal-500 duration-500" :to="{ name: 'help'}">
@@ -56,23 +57,7 @@ export default {
     const hideMenu = () => {
       isMenuVisible.value = false;
     };
-
-    const handleResize = () => {
-      if (window.innerWidth >= 768) {
-        isMenuVisible.value = true;
-      } else {
-        isMenuVisible.value = false;
-      }
-    };
-
-    onMounted(() => {
-      handleResize();
-      window.addEventListener('resize', handleResize);
-    });
-
-    onBeforeUnmount(() => {
-      window.removeEventListener('resize', handleResize);
-    });
+   
 
   return {    
     toggleMenu,

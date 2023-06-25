a982b9133640bbe7ee079c950165b27baa432586
Isaac Vieira <isaacvf.dev@gmail.com>
2023-06-25 11:48:45 -0300

    feat/10: ProductFeatures.vue is more responsive but not 100% yet. There were also changes in VslSection.vue in order to better see the responsive layout on ProductFeatures.vue

--- src/layout/section/ProductFeatures.vue
+++ src/layout/section/ProductFeatures.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="w-full bg-emerald-50">
+  <section class="w-full bg-emerald-50 px-4 py-8">
     <div
       
       class="max-w-5xl mx-[auto] pt-3 flex justify-center items-center flex-col"
@@ -10,65 +10,58 @@
         malesuada fames ac turpis egestas. Pellentesque ut rutrum purus.
       </p>
 
-      <div class="w-full flex justify-center items-center">
-        <div class="max-w-[30%] pr-8">
+      <div class=" lg:flex lg:justify-center lg:items-center">
+        <div class="max-w-[85%] lg:max-w-[30%] mx-10 px-4 lg:pr-8 flex lg:block lg:text-left text-sm lg:text-xl">
           <a-button
             :activeButton="isActive(1)"
-            @click="toggleButton(1)"
-            fontSize="xl"
-            textAlign="left"            
-            class="px-8 py-2 mt-3 mb-4 w-full"
+            @click="toggleButton(1)"         
+                      
+            class="lg:px-6 py-2 mt-3 mb-4"
           >
-            <i-fast-foward-ten class="mr-2" :width="25" :height="25" /> Nibh sagittis
+            <i-fast-foward-ten class="lg:mr-2" :width="25" :height="25" /> Nibh sagittis
           </a-button>
           <a-button
             :activeButton="isActive(2)"
-            @click="toggleButton(2)"
-            fontSize="xl"
-            textAlign="left"
-            class="px-8 py-2 mt-3 mb-4 w-full"
+            @click="toggleButton(2)"           
+            
+            class="lg:px-6 py-2 mt-3 mb-4"
           >
-            <i-rewind-ten class="mr-2" :width="25" :height="25" /> Suspendisse tortor
+            <i-rewind-ten class="lg:mr-2" :width="25" :height="25" /> Suspendisse tortor
           </a-button>
           <a-button
             :activeButton="isActive(3)"
-            @click="toggleButton(3)"
-            fontSize="xl"
-            textAlign="left"
-            class="px-8 py-2 mt-3 mb-4 w-full"
+            @click="toggleButton(3)"                        
+            class="lg:px-6 py-2 mt-3 mb-4"
           >
-            <i-pause class="mr-2" :width="25" :height="25" /> Sed enim tempus
+            <i-pause class="lg:mr-2"  :width="25" :height="25" /> Sed enim tempus
           </a-button>
           <a-button
             :activeButton="isActive(4)"
-            @click="toggleButton(4)"
-            fontSize="xl"
-            textAlign="left"
-            class="px-8 py-2 mt-3 mb-4 w-full"
+            @click="toggleButton(4)"                        
+            class="lg:px-6 py-2 mt-3 mb-4"
           >
-            <i-play class="mr-2" :width="25" :height="25" /> Aliquam volutpat
+            <i-play class="lg:mr-2"  :width="25" :height="25" /> Aliquam volutpat
           </a-button>
           <a-button
             :activeButton="isActive(5)"
             @click="toggleButton(5)"
-            fontSize="xl"
-            textAlign="left"
-            class="px-8 py-2 mt-3 mb-4 w-full"            
+                        
+            class="lg:px-6 py-2 mt-3 mb-4"            
           >
-            <i-volume-mute class="mr-2" :width="25" :height="25" /> Etiam metus
+            <i-volume-mute class="lg:mr-2" :width="25" :height="25" /> Etiam metus
           </a-button>
           <a-button
             :activeButton="isActive(6)"
             @click="toggleButton(6)"
-            fontSize="xl"
+            
             textAlign="left"
-            class="px-8 py-2 mt-3 mb-4 w-full"
+            class="lg:px-6 py-2 mt-3 mb-4"
           >
-            <i-volume-high class="mr-2" :width="25" :height="25" /> Volutpat metus
+            <i-volume-high class="lg:mr-2" :width="25" :height="25" /> Volutpat metus
           </a-button>
         </div>
         <iframe
-          class="ml-6 w-full h-96 bg-white"
+          class="my-8 px-4 lg:ml-6 w-full h-96 bg-white"
           src="https://www.youtube.com/embed/Q2-0qZoYBA0"
           title="Barulho de Chuva para Dormir Profundamente e Relaxar - Som de Chuva com Trovões - Rain sounds #61"
           frameborder="0"
@@ -107,15 +100,3 @@ export default {
 };
 </script>
 
-<style scoped>
-.active {
-  background-color: #10b981;
-  color: white;
-
-  transition: background-color 1000ms linear;
-}
-
-.active:hover {
-  background-color: #047857;
-}
-</style>

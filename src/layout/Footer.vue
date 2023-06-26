390db3d30937755101b59329ffe7f71bfa06a9a9
Isaac Vieira <isaacvf.dev@gmail.com>
2023-06-26 20:00:36 -0300

    feat/12: Added some responsiveess and hover effect to the Footer.vue

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -4,10 +4,10 @@
   >
     <nav class="flex justify-center mx-2 sm:mx-4 lg:mx-8 px- 1 sm:px-2 lg:px-4 py-4 font-semibold">
       <div>
-        <router-link class="rounded-lg px-2 sm:px-4 py-2 font-semibold" :to=" { name: 'terms-of-use'}">
+        <router-link class="rounded-lg text-sm sm:text-base hover:text-teal-500 duration-500 px-2 sm:px-4 py-2 font-semibold" :to=" { name: 'terms-of-use'}">
           Termos de Uso
         </router-link>
-        <router-link class="rounded-lg ml-2 sm:ml-4 px-2 sm:px-4 py-2 font-semibold" :to=" { name: 'privacy-policy'}">
+        <router-link class="rounded-lg ml-2 text-sm sm:text-base hover:text-teal-500 duration-500 sm:ml-4 px-2 sm:px-4 py-2 font-semibold" :to=" { name: 'privacy-policy'}">
           Políticas de Privacidade
         </router-link>
       </div>

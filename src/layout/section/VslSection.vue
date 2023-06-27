f031d40951090401770a3d57571e010eb746ac93
Paulo Melo <paulovictormmello@gmail.com>
2023-06-27 23:11:10 +0100

    feature(04): screen responsiveness.VSL vue

--- src/layout/section/VslSection.vue
+++ src/layout/section/VslSection.vue
@@ -1,15 +1,17 @@
 <template>
     <section style="height: calc(100vh - 3.5rem);" class="max-w-5xl flex justify-center items-center pt-3 mx-[auto]" >
-        <div>
-            <h1 class="font-bold text-5xl text-center py-4">Lorem ipsum dolor sit. <br> Scelerisque!</h1>
-            <p class="font-semibold	text-center py-2 mb-6">Nullam non facilisis risus, sed tempus leo. Aenean magna erat, sollicitudin in felis ac, maximus placerat orci.<br> In vel ligula justo.</p>
-            <iframe class="w-full h-96 bg-emerald-50 " src="https://www.youtube.com/embed/Q2-0qZoYBA0" title="Barulho de Chuva para Dormir Profundamente e Relaxar - Som de Chuva com Trovões - Rain sounds #61" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <div class="flex justify-center mt-6">
-                <a-button :paddingSize="8" fontSize="xl">
-                    Entrar na lista de espera
-                </a-button>
-            </div>
-        </div>
+        <div class="flex flex-col items-center px-4 py-8 md:w-2/3 md:mx-auto">
+         <h1 class="font-bold text-3xl md:text-5xl text-center md:py-4">Lorem ipsum dolor sit.<br>Scelerisque!</h1>
+            <p class="font-semibold text-center py-2 mb-6">Nullam non facilisis risus, sed tempus leo. Aenean magna erat, sollicitudin in felis ac, maximus placerat orci.<br>In vel ligula justo.</p>
+             <div class="aspect-w-16 aspect-h-9 md:w-full md:h-96">
+                <iframe class="w-full h-full" src="https://www.youtube.com/embed/Q2-0qZoYBA0" title="Barulho de Chuva para Dormir Profundamente e Relaxar - Som de Chuva com Trovões - Rain sounds #61" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+           </div>
+           <div class="mt-6">
+           <a-button class="px-6 py-2 text-base md:px-8 md:py-2 text-lg" style="padding: 0.5rem 1rem; font-size: 1.125rem;">
+            Entrar na lista de espera
+           </a-button>
+           </div>
+         </div>
     </section>
 </template>
 

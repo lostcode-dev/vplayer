c393c0e8d8b87b463a1665ce3574496404194bbb
Daniel Soares <daniel.soares@fortics.com.br>
2023-07-02 10:30:50 -0300

    documentation(22): Add state button in storybook.

--- src/atoms/Button.stories.tsx
+++ src/atoms/Button.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
 
 import Button from './Button.vue';
+import { ref } from 'vue';
 
 const meta = {
   component: Button,
@@ -15,12 +16,35 @@ type Story = StoryObj<typeof meta>;
  * See https://storybook.js.org/docs/vue/api/csf
  * to learn how to use render functions.
  */
-export const Primary: Story = {
+export const Default: Story = {
   render: (args) => ({
     components: { Button },
+    argTypes: {
+      backgroundColor: { control: 'color' },
+    },
     setup() {
       return {args};
     },
-    template: '<Button v-bind="args"> Click Here </Button',
+    template: '<Button v-bind="args"> Click Here </Button>',
+  }),  
+};
+
+export const StateButton: Story = {
+  render: (args) => ({
+    components: { Button },
+    setup() {
+      let activeButton = ref(1)
+
+      const toggleButton = (button: number) => {
+        activeButton.value = button
+      }
+  
+      const isActive = (buttonNumber: number) => {
+        return activeButton.value === buttonNumber
+      }
+
+      return {args, toggleButton, isActive};
+    },
+    template: '<Button v-bind="args" :activeButton="isActive(1)" @click="toggleButton(1)"> Nibh sagittis </span> </Button> </br> <Button v-bind="args" :activeButton="isActive(2)" @click="toggleButton(2)"> Suspendisse tortor </Button>',
   }),  
 };
